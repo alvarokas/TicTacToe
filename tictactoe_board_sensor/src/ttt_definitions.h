9432795483b19e5ba3848a751c6537b7dc18ab35
Alvaro Castro Gonzalez <acgonzal@ing.uc3m.es>
2013-09-04 18:55:06 -0400

    added the state board sensor. ready to be tested

--- tictactoe_board_sensor/src/ttt_definitions.h
+++ tictactoe_board_sensor/src/ttt_definitions.h
@@ -9,6 +9,8 @@ namespace ttt
 typedef std::vector<cv::Point> t_Cell;    // vector of points delimiting a cell
 typedef std::vector<t_Cell> t_Board;   // vector of cells, i.e. a vector of vectors of points
 
+typedef enum {empty=0, blue, red} t_Cell_State; // used to determine the three possible states of a cell
+
 }
 
 #endif // TTT_DEFINITIONS_H
